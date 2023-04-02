57537c432b4b6d489286198f2c70ed1d60370729
Peshkov Dmitry <peshkov-dv@yandex.ru>
2023-04-02 16:10:26 +0300

    64 и 66

---
+++ Task64/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от N до 1.
+
+string PrintNumbers(int n)
+{
+    if (n == 1) return 1.ToString();
+    return n.ToString() + ", " + PrintNumbers(n - 1);
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write(PrintNumbers(n));
---
+++ Task66/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдет сумму натуральных 
+// элементов в промежутке от M до N.
+
+int GetSum(int m, int n)
+{
+    if (m == n) return m;
+    return GetSum(m, n - 1) + n;
+}
+
+Console.Clear();
+Console.Write("Введите M и N: ");
+int[] numbers = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+Console.WriteLine($"{GetSum(numbers[0], numbers[1])}");
