9db61541521a6c8a5a0ccd54a888c9c76bfea01a
Peshkov Dmitry <peshkov-dv@yandex.ru>
2023-04-02 15:53:51 +0300

    empty

---
+++ prepare.cmd
@@ -0,0 +1,51 @@
+mkdir Task1
+
+mkdir Task2
+
+mkdir Task3
+
+mkdir Task4
+
+mkdir Task5
+mkdir Task6
+
+mkdir Task7
+
+mkdir Task8
+
+mkdir Task9
+
+mkdir Task10
+cd Task1
+dotnet new console
+cd .. 
+cd Task2
+dotnet new console
+cd .. 
+cd Task3
+dotnet new console
+cd .. 
+cd Task4
+dotnet new console
+cd .. 
+cd Task5
+dotnet new console
+cd .. 
+cd Task6
+dotnet new console
+cd .. 
+cd Task7
+dotnet new console
+cd .. 
+cd Task8
+dotnet new console
+cd .. 
+cd Task9
+dotnet new console
+cd .. 
+cd Task10
+dotnet new console
+cd .. 
+git init
+git add --all
+git commit -m "empty"
