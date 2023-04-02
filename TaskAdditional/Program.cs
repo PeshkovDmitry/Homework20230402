1b30436eccee5e850bb046a6cbd9e1d9e991626a
PeshkovDmitry <peshkov-dv@yandex.ru>
2023-04-02 20:05:49 +0300

    Вроде бы готово

--- TaskAdditional/Program.cs
+++ TaskAdditional/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пользователь вводит скобочные последовательности.
+// В последовательности могут встретится скобки вида: {},(),[]
+// Написать программу, которая определяет, правильная ли 
+// скобочная последовательность была введена
+// Правильная (),()[]{}
+// Неправильная (], ({)}
+
+bool Correct(string s)
+{
+    if (s == String.Empty) return true;
+    s = s.Replace("{}", "");
+    s = s.Replace("[]", "");
+    s = s.Replace("()", "");
+    if (s.Length > 0)
+    {
+        if (s.Contains("{}") || s.Contains("[]") || s.Contains("()"))
+            Correct(s);
+        else return false;
+    }
+    return true;
+}
+
+Console.Write("Введите последовательность: ");
+string str = Console.ReadLine();
+Console.WriteLine(Correct(str) ? "Последовательность верна" : "Последовательность не верна");
---
+++ TaskSqrt/Program.cs
@@ -0,0 +1,29 @@
+﻿int Sqrt(int n, ref int count)
+{
+    count = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        count++;
+        if (i * i <= n && (i + 1) * (i + 1) > n) return i;
+    }
+    return 0;
+}
+
+void SqrtPro(int n, int start, int end, ref int count)
+{
+    if (end > start + 1)
+    {
+        count++;
+        int i = start + (end - start) / 2;
+        if (i * i > n) SqrtPro(n, start, i, ref count);
+        else if ((i + 1) * (i + 1) < n) SqrtPro(n, i, end, ref count);
+        else Console.WriteLine($"Квадратный корень из {n} равен {i}, число итераций (рекурсия) {count}");
+    } 
+}
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+Console.WriteLine($"Квадратный корень из {n} равен {Sqrt(n, ref count)}, число итераций (цикл) {count}");
+count = 0;
+SqrtPro(n, 0, n, ref count);
